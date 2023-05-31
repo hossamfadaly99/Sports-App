1ff7f04aa33c191fffc1c3996675fe577f09624a
Hossam Fadaly <74186546+hossamfadaly99@users.noreply.github.com>
2023-05-31 07:23:39 +0300

    caching league

---
+++ Sports App/Model/LocalLeague.swift
@@ -0,0 +1,13 @@
+//
+//  LocalLeague.swift
+//  Sports App
+//
+//  Created by Hossam on 31/05/2023.
+//
+
+import Foundation
+struct LocalLeague {
+  var leagueName: String
+  var leagueImage: String
+  var leagueId: Int
+}
---
+++ Sports App/Services/Caching/DBManager.swift
@@ -0,0 +1,75 @@
+//
+//  DBManager.swift
+//  Sports App
+//
+//  Created by Hossam on 31/05/2023.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+class DBManager{
+    static let sharedLeagueDB = DBManager()
+
+  var leagueName: String?
+  var leagueImage: String?
+  var leagueId: Int?
+  var arrayOfLeagues: Array<LocalLeague>? = []
+  var nsManagedLeagues : [NSManagedObject] = []
+  let manager : NSManagedObjectContext!
+  let entity: NSEntityDescription!
+
+  private init(){
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    manager = appDelegate.persistentContainer.viewContext
+    entity = NSEntityDescription.entity(forEntityName: "LeagueEntity", in: manager)
+  }
+
+  func insert(newLeague: LocalLeague){
+    let league = NSManagedObject(entity: entity!, insertInto: manager)
+    league.setValue(newLeague.leagueName, forKey: "league_name")
+    league.setValue(newLeague.leagueImage, forKey: "league_image")
+    league.setValue(newLeague.leagueId, forKey: "league_id")
+
+    do {
+      try manager.save()
+      print("saved")
+    } catch let error as NSError{
+      print(error)
+    }
+  }
+
+  func getAllLeagues() -> Array<LocalLeague>? {
+    arrayOfLeagues = []
+    let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "LeagueEntity")
+
+    do{
+      nsManagedLeagues = try manager.fetch(fetchRequest)
+
+      for league in nsManagedLeagues{
+        var leagueObj = LocalLeague(leagueName: "", leagueImage: "", leagueId: 0)
+        leagueObj.leagueId = league.value(forKey: "league_id") as! Int
+        leagueObj.leagueName = league.value(forKey: "league_name") as! String
+        leagueObj.leagueImage = league.value(forKey: "league_image") as! String
+        arrayOfLeagues?.append(leagueObj)
+      }
+      return  arrayOfLeagues
+    } catch let error as NSError{
+      print(error)
+      return []
+    }
+  }
+
+  func delete(id: Int) {
+
+      manager.delete(nsManagedLeagues[id])
+      do{
+          try manager.save()
+          print("Deleted!")
+      }catch let error{
+          print(error.localizedDescription)
+      }
+  }
+
+}
