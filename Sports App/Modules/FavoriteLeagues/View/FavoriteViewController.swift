d562998fc86570b8810adeaab06f37da50c54104
Hossam Fadaly <74186546+hossamfadaly99@users.noreply.github.com>
2023-05-31 08:01:07 +0300

    favorite UI

---
+++ Sports App/Modules/FavoriteLeagues/View/FavoriteViewController.swift
@@ -0,0 +1,68 @@
+//
+//  FavoriteViewController.swift
+//  Sports App
+//
+//  Created by Hossam on 31/05/2023.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+  @IBOutlet weak var tablee: UITableView!
+
+  var viewModel: FavoriteViewModel!
+  override func viewWillAppear(_ animated: Bool) {
+    self.tablee.reloadData()
+  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      viewModel = FavoriteViewModel()
+
+      viewModel.bindDBToViewController = { [weak self] in
+        self?.tablee.reloadData()
+
+      }
+
+      viewModel.getAllFavLeagues()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
+
+extension FavoriteViewController: UITableViewDelegate{
+
+}
+extension FavoriteViewController: UITableViewDataSource{
+  func numberOfSections(in tableView: UITableView) -> Int {
+    1
+  }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    viewModel.allDBLeagues.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "FavLeaguesTableViewCell") as! LeaguesTableViewCell
+
+    cell.leagueLabel.text = viewModel.allDBLeagues[indexPath.row].leagueName
+
+    cell.leagueImage.kf.setImage(with: URL(string: viewModel.allDBLeagues[indexPath.row].leagueImage))
+    
+
+    return cell
+  }
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    let leagueDetailsVC = self.storyboard?.instantiateViewController(identifier: "LeagueDetailsViewController") as! LeagueDetailsViewController
+//    leagueDetailsVC.sportName = self.sportName
+    leagueDetailsVC.leagueId = (viewModel.allDBLeagues[indexPath.row].leagueId)
+    leagueDetailsVC.leagueName = (viewModel.allDBLeagues[indexPath.row].leagueName)
+    leagueDetailsVC.leagueImage = (viewModel.allDBLeagues[indexPath.row].leagueImage)
+    self.present(leagueDetailsVC, animated: true)
+  }
+
+
+}
---
+++ Sports App/Modules/FavoriteLeagues/ViewModel/FavoriteViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  FavoriteViewModel.swift
+//  Sports App
+//
+//  Created by Hossam on 31/05/2023.
+//
+
+import Foundation
+class FavoriteViewModel{
+
+  let database = DBManager.sharedLeagueDB
+
+  var bindDBToViewController: (()->()) = {}
+
+  var allDBLeagues: [LocalLeague] = [] {
+    didSet{
+      bindDBToViewController()
+    }
+  }
+
+//  var isFavLeague: Bool = false {
+//    didSet{
+//      bindDBToViewController()
+//    }
+//  }
+
+
+
+  func deleteFavLeague(leagueId: Int){
+    getAllFavLeagues()
+    for (index, item) in allDBLeagues.enumerated(){
+      if item.leagueId == leagueId{
+        database.delete(id: index)
+        break
+      }
+    }
+//    isFavLeague = false
+    getAllFavLeagues()
+  }
+
+  func getAllFavLeagues(){
+      allDBLeagues =  database.getAllLeagues() ?? []
+  }
+}
